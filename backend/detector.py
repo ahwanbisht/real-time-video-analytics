673793d4237f2bf3942fa2b6228541c488b3b91d
Ahawan Bisht <ahwanbisht213@gmail.com>
2026-02-04 13:18:06 +0530

    Phase 1 complete.

---
+++ backend/detector.py
@@ -0,0 +1,28 @@
+from ultralytics import YOLO
+from config import CONFIDENCE_THRESHOLD
+
+class Detector:
+    def __init__(self):
+        self.model = YOLO("yolov8n.pt")
+
+    def detect(self, frame):
+        results = self.model(frame, verbose = False)
+
+        detections = []
+        #loop through all objects and extract each object (box) class id
+        for result in results:
+            for box in result.boxes:
+                cls = int(box.cls[0])
+
+                #yolo is pretrained on COCO dataset, class id 0 is for person
+                if cls == 0:
+                    conf = float(box.conf[0])
+                    if conf > CONFIDENCE_THRESHOLD:
+                        x1, y1, x2, y2 = box.xyxy[0]
+                        w = x2 - x1
+                        h = y2 - y1
+                        detections.append(
+                            ([float(x1), float(y1), float(w), float(h)], conf, 'person')
+                        )
+
+        return detections
---
+++ backend/main.py
@@ -0,0 +1,55 @@
+import cv2
+import time
+from detector import Detector
+from tracker import Tracker
+from config import FRAME_WIDTH, FRAME_HEIGHT, PROCESS_EVERY_N_FRAMES
+
+detector = Detector()
+tracker = Tracker()
+
+cap = cv2.VideoCapture(0)
+
+frame_count = 0
+
+prev_time = 0
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = cv2.resize(frame, (FRAME_WIDTH, FRAME_HEIGHT))
+    frame_count += 1
+
+    if frame_count % PROCESS_EVERY_N_FRAMES != 0:
+        continue
+
+    detections = detector.detect(frame)
+    tracks = tracker.update(detections, frame)
+
+    for track in tracks:
+        if not track.is_confirmed():
+            continue
+
+        track_id = track.track_id
+        l, t, r, b = track.to_ltrb()
+
+        cv2.rectangle(frame, (int(l), int(t)), (int(r), int(b)), (0, 255, 0), 2)
+        cv2.putText(frame, f"ID: {track_id}", (int(l), int(t - 10)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+    # Calculate and display FPS
+    current_time = time.time()
+    fps = 1 / (current_time - prev_time) if prev_time != 0 else 0
+    prev_time = current_time
+    cv2.putText(frame, f"FPS: {int(fps)}", (20, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)    
+
+
+    cv2.imshow("Retail Analytics - Phase 1", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
